f6415f8df25ebc739585831c5e02509e507a3c7a
Justin Du <justin.d128@gmail.com>
2018-11-20 11:51:35 -0500

    Add the Player Model
    * Admin model is there as well, can filter, search, edit

--- ccms/cc_management/admin.py
+++ ccms/cc_management/admin.py
@@ -6,3 +6,11 @@ from .models import *
 # Register your models here.
 admin.site.register(Club)
 admin.site.register(Club_Classifications)
+
+# Display all fields for player
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'classification', 'club_id', 'grade', 'rating', 'uscf_id', 'uscf_rating', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('club_id', 'classification', 'is_active', 'grade')
+    # TODO - Figure out what else we want to search for
+    search_fields = ['first_name', 'last_name']
+admin.site.register(Player, PlayerAdmin)
--- ccms/cc_management/models.py
+++ ccms/cc_management/models.py
@@ -14,3 +14,66 @@ class Club_Classifications(models.Model):
     # Define how it's displayed
     def __str__(self):
         return self.name
+
+# Player Model
+class Player(models.Model):
+    # Club ID
+    club_id = models.ForeignKey(Club, on_delete=models.CASCADE)
+
+    # Classficiation, default is 1, which is pawns
+    classification = models.ForeignKey(Club_Classifications, on_delete=models.CASCADE, default=1)
+
+    # Created at and Updated at fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    first_name = models.CharField(max_length=100)
+    # Middle Names are optional
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100)
+
+    # USCF is optional
+    uscf_id = models.IntegerField(blank=True, null=True)
+    uscf_rating = models.IntegerField(blank=True, null=True)
+    # Rating is mandatory, default 0
+    rating = models.IntegerField(default=0)
+
+    # Players can be made inactive
+    is_active = models.BooleanField(default=True)
+
+    # Grade is mandatory
+    KINDERGARTEN = 0
+    FIRST = 1
+    SECOND = 2
+    THIRD = 3
+    FOURTH = 4
+    FIFTH = 5
+    SIXTH = 6
+    SEVENTH = 7
+    EIGHTH = 8
+    NINTH = 9
+    TENTH = 10
+    ELEVENTH = 11
+    TWELFTH = 12
+    GRADE_CHOICES = (
+        (KINDERGARTEN, 'Kintergarten'),
+        (FIRST, '1st'),
+        (SECOND, '2nd'),
+        (THIRD, '3rd'),
+        (FOURTH, '4th'),
+        (FIFTH, '5th'),
+        (SIXTH, '6th'),
+        (SEVENTH, '7th'),
+        (EIGHTH, '8th'),
+        (NINTH, '9th'),
+        (TENTH, '10th'),
+        (ELEVENTH, '11th'),
+        (TWELFTH, '12th'),
+    )
+    grade = models.IntegerField(choices=GRADE_CHOICES, blank=True, null=True)
+
+    # TODO - Can classify people by elementary, middle
+    """
+    def is_upperclass(self):
+        return self.year_in_school in (self.JUNIOR, self.SENIOR)
+    """
