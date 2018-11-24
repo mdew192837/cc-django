9e8f142c805164938164a3c38d82ea3485d7e862
mdew192837 <justin.d128@gmail.com>
2018-11-24 12:46:47 -0500

    Add Password Request Feature

--- ccms/ccms/settings.py
+++ ccms/ccms/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = '3kr%wmb-37g$qseuw4jt*7bq80!qo)9+lk%c$c7c_l*note625'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['104.248.50.78']
+ALLOWED_HOSTS = ['104.248.50.78', '127.0.0.1']
 
 
 # Application definition
@@ -138,3 +137,11 @@ LOGIN_REDIRECT_URL = 'club_list'
 
 # Login URL for @login_required pages
 LOGIN_URL = 'login'
+
+# E-mail Stuff
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
