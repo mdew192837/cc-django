3fb3600a08c0f48bc0ce17b179270ed8cdf8439f
mdew192837 <justin.d128@gmail.com>
2018-11-24 17:55:47 -0500

    Update Configs for E-mail

--- ccms/ccms/settings.py
+++ ccms/ccms/settings.py
@@ -140,8 +140,9 @@ LOGIN_URL = 'login'
 
 # E-mail Stuff
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
