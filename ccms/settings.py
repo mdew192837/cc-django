532c4446075507a12aad9bcb25967e2cfac7b63f
mdew192837 <justin.d128@gmail.com>
2018-11-26 21:02:37 -0500

    Add Static Files from staticcollect

--- ccms/settings.py
+++ ccms/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 
