0f409f0d371125e7f02f6e7a7ff0f0067085aa54
mdew192837 <justin.d128@gmail.com>
2018-11-26 20:26:27 -0500

    Update WSGI

--- ccms/wsgi_old.py
+++ ccms/wsgi_old.py
@@ -12,6 +12,8 @@ import os
 import dotenv
 from django.core.wsgi import get_wsgi_application
 
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccms.settings')
 
 application = get_wsgi_application()
