da7bd272317da0b862cc686bb474007c7813ed57
mdew192837 <justin.d128@gmail.com>
2018-11-26 20:22:27 -0500

    Make new versions of files

--- manage_old.py
+++ manage_old.py
@@ -5,6 +5,8 @@ import sys
 import dotenv
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccms.settings')
     try:
         from django.core.management import execute_from_command_line
