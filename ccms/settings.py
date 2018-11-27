244c3f68216a17580d52e5a79c3b95ed813eebd1
mdew192837 <justin.d128@gmail.com>
2018-11-26 20:12:37 -0500

    Update .gitignore and settings.py

--- ccms/settings.py
+++ ccms/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3kr%wmb-37g$qseuw4jt*7bq80!qo)9+lk%c$c7c_l*note625'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
